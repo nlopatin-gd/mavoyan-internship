eb928990ddb9a402c16a3c831ad3714434483715
avmang <mavoyan@griddynamics.com>
2025-03-05 14:33:44 +0400

    fixed root main.rf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,28 @@
 module "network" {
-  source         = "./modules/network"
-  instance_group = module.compute.instance_group
+  source = "./modules/network"
 }
 
-module "compute" {
+module "compute1" {
   source     = "./modules/compute"
   network_id = module.network.network_id
   subnet_id  = module.network.subnet_id
+  name       = "mavoyan1"
+  zones = ["us-east1-d", "us-east1-b"]
+  startup_script = "sudo apt update\nsudo apt install -y apache2\nsudo systemctl start apache2\nsudo systemctl enable apache2\nsudo apt install php libapache2-mod-php -y\nsudo rm /var/www/html/index.html\nsudo touch /var/www/html/index.php\necho \"<?php echo 'Hostname: ' . gethostname(); ?>\" | sudo tee /var/www/html/index.php\nsudo systemctl restart apache2"
+  depends_on = [module.network]
 }
+
+
+module "compute2" {
+  source     = "./modules/compute"
+  name       = "mavoyan2"
+  zones = ["us-east1-c", "us-east1-d"]
+  startup_script = "sudo apt update\nsudo apt install -y apache2\nsudo systemctl start apache2\nsudo systemctl enable apache2\nsudo apt install php libapache2-mod-php -y\nsudo rm /var/www/html/index.html\nsudo touch /var/www/html/index.php\necho \"<?php echo 'Script2 - Hostname: ' . gethostname(); ?>\" | sudo tee /var/www/html/index.php\nsudo systemctl restart apache2"
+  network_id = module.network.network_id
+  subnet_id  = module.network.subnet_id
+  depends_on = [module.network]
+}
+
 
 resource "google_storage_bucket" "default" {
   name     = "mavoyan-terraform-remote-backend"
@@ -22,15 +37,3 @@ resource "google_storage_bucket" "default" {
   }
 }
 
-resource "local_file" "default" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
-
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.default.name}"
-    }
-  }
-  EOT
-}
